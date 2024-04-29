bcf4bfb7fbdc06d98929d838175da64166f443a3
LuminanceRaven <168450718+LuminanceRaven@users.noreply.github.com>
2024-04-29 21:26:28 +0300

    Update shell.py
    lolololo

--- shell.py
+++ shell.py
@@ -1,8 +1,8 @@
 import lamp
 
 while True:
-    text = input('lamp >')
-    result, error = lamp.run('<stdin>', text)
+		text = input('lamp > ')
+		result, error = lamp.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+		if error: print(error.as_string())
+		else: print(result)
